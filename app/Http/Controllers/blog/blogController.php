af0efc670ec49dcdff37609be7f4f90d56b0bdba
Dario Zambrano <69017697+dariozambra97@users.noreply.github.com>
2020-08-05 18:18:49 -0500

    Update blogController.php

--- app/Http/Controllers/blog/blogController.php
+++ app/Http/Controllers/blog/blogController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\blog;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Blog;
+
 
 class blogController extends Controller
 {
@@ -11,4 +13,47 @@ class blogController extends Controller
     {
 
     }
+    public function index()
+    {
+        return view('blog.crear');
+    }
+    public function create()
+    {
+        
+    }
+    public function store(Request $request)
+    {
+    	$data = request()->validate([
+    		'titulo' => 'required|max:255',
+    		'descripcion' =>'required|max:500',
+    		'foto' => 'required|max:2',
+    	]);
+
+    	$user = auth()->user();
+    	$blog = new Blog();
+    	$blog->titulo = request('titulo');
+    	$blog->descripcion = request('descripcion');
+    	$blog->foto = request('foto');
+		$blog->userId = $user->id;
+
+		$blog->save();
+
+		return redirect('home');
+    	
+    }
+    public function edit($value)
+    {
+    	$blog = Blog::where('id', $value)->first();
+        return view('blog.edit',
+                ['blog' => $blog],
+            );
+    }
+    public function update( Request $request, $id)
+    {
+    	
+	    Blog::where('id', $id)->update($request->all());
+	    return redirect('blog.crear');
+    }
+    
 }
+	
