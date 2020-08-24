2c7b1559d072d5d9e3b12851918f8642b654c3d8
Dario Zambrano <69017697+dariozambra97@users.noreply.github.com>
2020-08-24 07:22:25 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,16 +8,39 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                <table id="mytable" class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Título</th>
+                            <th>descripcion</th>
+                            <th>foto</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($blogs as $blog)
+                            <tr>
+                                <td>{{ $blog->id }}</td>
+                                <td>{{ $blog->titulo }}</td>
+                                <td>{{ $blog->descripcion }}</td>
+                                <td>{{ $blog->foto }}</td>
+                                <td><a class="btn btn-outline-info" href="/crearBlog/{{ $blog['id'] }}/edit">Editar</a></td>
+                                <td><a class="btn btn-outline-info" href="#" data-toggle="modal" data-target="#deleteModal">Eliminar</a></td>
 
-                    {{ __('You are logged in!') }}
+                                
+                            </tr>
+
+                        @endforeach
+                        
+                    </tbody>
+                </table>
                 </div>
             </div>
+        </div> 
+        <div class="col-md-4">
+            <a class="btn btn-success" href="{{url('crearBlog')}}">Crear Blog</a>
         </div>
     </div>
 </div>
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby=example></div>
 @endsection
