8eeae4d377cce6d536c9c84cf9f366760bdd6471
Dario Zambrano <69017697+dariozambra97@users.noreply.github.com>
2020-08-24 07:23:20 -0500

    Update mis_blog.blade.php

--- resources/views/mis_blog.blade.php
+++ resources/views/mis_blog.blade.php
@@ -4,21 +4,17 @@
 <div class="container">
 <div class="overlay"></div>
 <div class="nav-scroller py-1 mb-2">
-     <nav class="nav d-flex justify-content-between">
-        <a class="p-2 text-muted" href="#">World</a>
-        <a class="p-2 text-muted" href="#">U.S.</a>
-        <a class="p-2 text-muted" href="#">Technology</a>
-        <a class="p-2 text-muted" href="#">Design</a>
-        <a class="p-2 text-muted" href="#">Culture</a>
-        <a class="p-2 text-muted" href="#">Business</a>
-        <a class="p-2 text-muted" href="#">Politics</a>
-        <a class="p-2 text-muted" href="#">Opinion</a>
-        <a class="p-2 text-muted" href="#">Science</a>
-        <a class="p-2 text-muted" href="#">Health</a>
-        <a class="p-2 text-muted" href="#">Style</a>
-        <a class="p-2 text-muted" href="#">Travel</a>
-        <a class="p-2 text-muted" href="#">Mis Blogs</a>
-    </nav>
+     nav class="nav d-flex justify-content-between">
+          <a class="p-2 text-muted" href="#">Deporte</a>
+          <a class="p-2 text-muted" href="#">Salud</a>
+          <a class="p-2 text-muted" href="#">Tecnologia</a>
+          <a class="p-2 text-muted" href="#">Noticia</a>
+          <a class="p-2 text-muted" href="#">Economia</a>
+          <a class="p-2 text-muted" href="#">Familia</a>
+          <a class="p-2 text-muted" href="#">Politica</a>
+          <a class="p-2 text-muted" href="#">Ciencia</a>
+          <a class="p-2 text-muted" href="#">Mis Blogs</a>
+        </nav>
 </div>
 
 <div class="container">
